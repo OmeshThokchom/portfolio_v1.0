51dc7c4385562ccab0d2b67ab13c14eb881628e1
OmeshThokchom <thokchomomesh@gmail.com>
2025-09-03 21:33:03 +0530

    update jsx background #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import DarkVeil from './components/Darkveil'
+import DarkVeil from "./components/DarkVeil";
+import ParticlesBackground from "./components/Particles";
 
 function App() {
   return (
-    <div style={{ 
-      width: '100vw',       // full width of viewport
-      height: '100vh',      // full height of viewport
-      position: 'relative', // so DarkVeil layers properly
-      overflow: 'hidden'    // avoids scrollbars on small screens
-    }}>
-      <DarkVeil />
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      {/* Background 1 - DarkVeil */}
+      <div style={{ position: "absolute", inset: 0, zIndex: 0 }}>
+        <DarkVeil />
+      </div>
+
+      {/* Background 2 - Particles (in front of DarkVeil) */}
+      <div style={{ position: "absolute", inset: 0, zIndex: 1 }}>
+        <ParticlesBackground />
+      </div>
+
+      {/* Foreground Content */}
+      <div
+        style={{
+          position: "relative",
+          zIndex: 2,
+          color: "#fff",
+          textAlign: "center",
+          paddingTop: "40vh",
+        }}
+      >
+        <h1>🔥 My Portfolio 🔥</h1>
+        <p>React + DarkVeil + Particles</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
